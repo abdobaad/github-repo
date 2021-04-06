15541856f9acfa4923992c6997a4425e19e2d671
abdobaad <you@example.com>
2021-04-06 22:51:28 +0100

    restyling the repo & adding media queries

--- Components/RepoCard/index.jsx
+++ Components/RepoCard/index.jsx
@@ -1,26 +1,29 @@
 import moment from 'moment';
 import React from 'react';
-import Link from "next/link"
+import Link from "next/link";
+import { TextMaxLength } from '../../helpers';
 
 const RepoCard = ({repo}) => {
     const {owner,name,description,stargazers_count,open_issues_count,created_at,html_url} = repo;
     return (
-        <Link  href={`${html_url}`} >
-            <a target="_blank">
-            <div className="repo_card--container"> 
+       
+        <div className="repo_card--container"> 
             <img className="creator_image" src={owner.avatar_url} alt={owner.login} />
             <div className="card_info">
-              <h2>{name && name.length > 40 ? `${name.slice(0,50)}...` : name }</h2>
-              <p className="description">{description && description.length > 120 ?  `${description.slice(0,120)}...` : description }</p>
-              <div className="repo--data">
-                  <span className="stars">Stars : {stargazers_count}</span>
-                  <span className="issues">Issue{open_issues_count === 1 ? '':'s' } : {open_issues_count}</span>
-                  <p className="created_date">{moment((created_at)).startOf('day').fromNow()}</p>
-              </div>
+                <Link  href={`${html_url}`} >
+                  <a target="_blank">
+                    <h2>{TextMaxLength(name,50)}</h2>
+                  </a>
+                </Link>
+                <p className="description">{description && TextMaxLength(description,120)}</p>
+                <div className="repo--data">
+                    <span className="stars">Stars : {stargazers_count}<img src="/star.svg" alt="star" className="icon"/> </span>
+                    <span className="issues">Issue{open_issues_count === 1 ? '':'s' } : {open_issues_count}</span>
+                    <p className="created_date">{moment((created_at)).startOf('day').fromNow()}</p>
+                </div>
             </div>
         </div>
-        </a>
-        </Link>
+      
     );
 };
 
---
+++ helpers.js
@@ -0,0 +1,1 @@
+export const TextMaxLength = (text,max) => text.length > max ? `${text.slice(0,max)}...`: text
